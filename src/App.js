d50ab87d051e416647ea3d35f150bc6055cee4d2
arca-ane <99656977+arca-ane@users.noreply.github.com>
2022-12-13 19:37:48 +0530

    Added Photos

--- src/App.js
+++ src/App.js
@@ -27,6 +27,37 @@ import techkriti from './images/techkriti.jpeg';
 import takneek from './images/Takneek.jpg';
 import summer from './images/Summer-project.png';
 import bgvideo from './images/team-bg.mp4';
+import balsa from './images/balsa.jpg';
+
+//Team Photos
+
+import Aakansha from './images/team/Aakanksha.jpg';
+import Abhinaba from './images/team/Abhinaba.jpg';
+import Aditya from './images/team/Aditya.jpg';
+import Alka from './images/team/Alka.jpg';
+import Ambuj from './images/team/Ambuj.jpg';
+import Arpit from './images/team/Arpit.jpg';
+import Himanshu from './images/team/Himanshu.jpg';
+import Mohit from './images/team/Mohit.jpeg';
+import Moin from './images/team/Moin.jpg';
+import Netraj from './images/team/Netraj.jpg';
+import Omkar from './images/team/Omkar.jpeg';
+import Piyush from './images/team/Piyush.jpg';
+import Prabuddha from './images/team/Prabuddha.png';
+import Pranshu from './images/team/Pranshu.jpg';
+import Pulak from './images/team/Pulak.png';
+import Rahul from './images/team/Rahul.jpg';
+import Rishima from './images/team/Rishima.jpg';
+import Ritwik from './images/team/Ritwik.jpg';
+import Sarthak from './images/team/Sarthak.jpg';
+import Shrey from './images/team/Shrey.jpg';
+import Shreyash from './images/team/Shreyash.jpg';
+import Sujeet from './images/team/Sujeet.jpg';
+import Ujjwal from './images/team/Ujjwal.jpeg';
+import Vaibhav from './images/team/Vaibhav.jpg';
+
+
+
 
 
 // import { createRoot } from "react-dom/client";
@@ -57,13 +88,13 @@ function App() {
               <Glance slide1={slide1} slide2={slide2} slide3={slide3} />
             </Route>
             <Route path='/projects'>
-              <Project bgvideo={bgvideo} techkriti={techkriti} />
+              <Project bgvideo={bgvideo} balsa={balsa} />
             </Route>
             <Route path='/activities'>
               <Activities aeroweek={aeroweek} techkriti={techkriti} summer={summer} takneek={takneek} bgvideo={bgvideo} />
             </Route>
             <Route path='/team'>
-              <Team bgvideo={bgvideo} techkriti={techkriti} insta={insta} fb={fb} envelope={envelope} />
+              <Team bgvideo={bgvideo} techkriti={techkriti} insta={insta} fb={fb} envelope={envelope} Aakansha={Aakansha} Abhinaba={Abhinaba} Aditya = {Aditya} Alka={Alka} Ambuj={Ambuj} Arpit={Arpit} Himanshu={Himanshu} Mohit={Mohit} Moin={Moin} Netraj={Netraj} Omkar={Omkar} Piyush={Piyush} Prabuddha={Prabuddha} Pranshu={Pranshu} Pulak={Pulak} Rahul={Rahul} Rishima={Rishima} Ritwik={Ritwik} Sarthak={Sarthak} Shrey={Shrey} Shreyash={Shreyash} Sujeet={Sujeet} Ujjwal={Ujjwal} Vaibhav={Vaibhav} />
             </Route>
           </Switch>
           </div>
